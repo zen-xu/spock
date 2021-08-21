4e98cf7a007702fb53a4c3792a8fab1187a143ff
ZhengYu, Xu <zen-xu@outlook.com>
2021-08-21 19:47:46 +0800

    feat: implement `ParamTable` (#5)

---
+++ src/_spock/param_table.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from collections import defaultdict
+from itertools import cycle
+from typing import Any
+from typing import DefaultDict
+from typing import Iterable
+from typing import Iterator
+from typing import List
+from typing import Optional
+
+from .parameter import Parameter
+
+
+class ParamTable(Iterable):
+    def __init__(self) -> None:
+        self.columns: List[List[str]] = [[]]
+        self.arguments_mapping: DefaultDict[str, List[Any]] = defaultdict(list)
+        self.current_columns_generate: Optional[cycle[str]] = None
+
+    def __truediv__(self, param: Parameter) -> ParamTable:
+        if self.current_columns_generate is not None:
+            self.current_columns_generate = None
+            self.columns.append([])
+
+        self.columns[-1].append(param.__name__)
+        return self
+
+    def __or__(self, arg: Any) -> ParamTable:
+        if self.current_columns_generate is None:
+            self.current_columns_generate = cycle(self.columns[-1])
+
+        column_name = next(self.current_columns_generate)
+        self.arguments_mapping[column_name].append(arg)
+        return self
+
+    def __iter__(self) -> Iterator[Any]:
+        max_len = max(len(args) for args in self.arguments_mapping.values())
+        for i in range(max_len):
+            items = []
+            for arguments in self.arguments_mapping.values():
+                try:
+                    arg = arguments[i]
+                except IndexError:
+                    arg = None
+                items.append(arg)
+            yield tuple(items)
---
+++ tests/test_param_table.py
@@ -0,0 +1,64 @@
+import pytest
+
+from _spock.param_table import ParamTable
+from _spock.parameter import Parameter
+
+
+@pytest.fixture(scope="function")
+def table():
+    return ParamTable()
+
+
+@pytest.fixture(scope="function")
+def a():
+    return Parameter("a")
+
+
+@pytest.fixture(scope="function")
+def b():
+    return Parameter("b")
+
+
+@pytest.fixture(scope="function")
+def c():
+    return Parameter("c")
+
+
+class TestParamTable:
+    def test_declare_header(self, table: ParamTable, a: Parameter, b: Parameter, c: Parameter):
+        table / a / b / c
+        assert table.columns[-1] == ["a", "b", "c"]
+
+    def test_add_args(self, table: ParamTable, a: Parameter, b: Parameter, c: Parameter):
+        table / a / b / c
+        table | 1 | 2 | 3
+        table | 4 | 5 | 6
+
+        assert table.arguments_mapping["a"] == [1, 4]
+        assert table.arguments_mapping["b"] == [2, 5]
+        assert table.arguments_mapping["c"] == [3, 6]
+
+        assert list(table) == [(1, 2, 3), (4, 5, 6)]
+
+    def test_declare_header_in_multi_sections(self, table: ParamTable, a: Parameter, b: Parameter, c: Parameter):
+        table / a / b  # section 1
+        table | 1 | 2
+        table | 4 | 5
+        table / c  # section 2
+        table | 3
+        table | 6
+
+        assert table.arguments_mapping["a"] == [1, 4]
+        assert table.arguments_mapping["b"] == [2, 5]
+        assert table.arguments_mapping["c"] == [3, 6]
+
+        assert list(table) == [(1, 2, 3), (4, 5, 6)]
+
+    def test_omit_args(self, table: ParamTable, a: Parameter, b: Parameter, c: Parameter):
+        table / a / b  # section 1
+        table | 1 | 2
+        table | 4 | 5
+        table / c  # section 2
+        table | 3
+
+        assert list(table) == [(1, 2, 3), (4, 5, None)]
