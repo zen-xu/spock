8d8fc616f2038a9e58c457bc0d5236784c2a7b56
ZhengYu, Xu <zen-xu@outlook.com>
2021-08-26 01:22:54 +0800

    fix: fix `get_functions_in_function` failed in py3.7

--- src/_spock/helper.py
+++ src/_spock/helper.py
@@ -1,10 +1,14 @@
 import inspect
+import sys
 
 from typing import Any
 from typing import Callable
 from typing import Dict
 
 from _pytest._code.code import Code
+
+
+LESS_PY38 = sys.version_info <= (3, 8)
 
 
 def get_functions_in_function(
@@ -24,11 +28,12 @@ def get_functions_in_function(
     body_statement_lineno = 0
     while True:
         statement = source.getstatement(body_statement_lineno).deindent()
-        if str(statement).startswith("def "):
+        if any(["def " in line for line in statement.lines]):
+            body_statement_lineno += len(statement.lines)
             break
         body_statement_lineno += 1
 
-    body_firstlineno = source.getstatementrange(body_statement_lineno)[1]
+    body_firstlineno = source.getstatementrange(body_statement_lineno)[0]
     body = source[body_firstlineno:].deindent()
     co = compile(str(body), str(filename), "exec")
 
@@ -36,6 +41,27 @@ def get_functions_in_function(
     context = {k: v for k, v in context.items() if inspect.isfunction(v)}
     for f in context.values():
         f_firstlineno = f.__code__.co_firstlineno + firstlineno
-        f.__code__ = f.__code__.replace(co_filename=str(filename), co_firstlineno=f_firstlineno + body_firstlineno)
+        if LESS_PY38:
+            from types import CodeType
+
+            f.__code__ = CodeType(
+                f.__code__.co_argcount,
+                f.__code__.co_kwonlyargcount,
+                f.__code__.co_nlocals,
+                f.__code__.co_stacksize,
+                f.__code__.co_flags,
+                f.__code__.co_code,
+                f.__code__.co_consts,
+                f.__code__.co_names,
+                f.__code__.co_varnames,
+                str(filename),  # type: ignore
+                f.__code__.co_name,
+                f_firstlineno + body_firstlineno,
+                f.__code__.co_lnotab,
+                f.__code__.co_freevars,
+                f.__code__.co_cellvars,
+            )
+        else:
+            f.__code__ = f.__code__.replace(co_filename=str(filename), co_firstlineno=f_firstlineno + body_firstlineno)
 
     return context
