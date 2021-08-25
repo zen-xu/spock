cef62dca385976ac8560c7760f919b33e21a6e8f
ZhengYu, Xu <zen-xu@outlook.com>
2021-08-26 01:27:01 +0800

    test: assert function file path generated from `get_functions_in_function`

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -26,6 +26,7 @@ def test_get_functions_in_function():
         funcs["c"]()
 
     assert funcs["a"].__code__.co_firstlineno == func1.__code__.co_firstlineno + 1
+    assert funcs["a"].__code__.co_filename == __file__
 
 
 class Class:
@@ -56,16 +57,19 @@ def test_get_functions_in_method():
     obj = Class()
     funcs = get_functions_in_function(obj.a)
     assert funcs["func"]() == 1 + obj.data1
-    assert funcs["func"].__code__.co_firstlineno == 36
+    assert funcs["func"].__code__.co_firstlineno == 37
+    assert funcs["func"].__code__.co_filename == __file__
 
 
 def test_get_functions_in_classmethod():
     funcs = get_functions_in_function(Class.b)
     assert funcs["func"]() == [3] + Class.data2
-    assert funcs["func"].__code__.co_firstlineno == 46
+    assert funcs["func"].__code__.co_firstlineno == 47
+    assert funcs["func"].__code__.co_filename == __file__
 
 
 def test_get_functions_in_staticmethod():
     funcs = get_functions_in_function(Class.c)
     assert funcs["func"]() == "abc"
-    assert funcs["func"].__code__.co_firstlineno == 51
+    assert funcs["func"].__code__.co_firstlineno == 52
+    assert funcs["func"].__code__.co_filename == __file__
